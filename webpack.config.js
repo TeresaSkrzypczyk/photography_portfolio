16301749831e9fb96dd64a029ad0329bd54255cb
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-07-08 13:35:49 +0200

    file to config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const entryFile = "index.js";
+
+module.exports = {
+    entry: `.js/${entryFile}`,
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, "build")
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "/"),
+        publicPath: "/build/",
+        compress: true,
+        port: 3001,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            }
+        ]
+    }
+};
