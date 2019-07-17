f5ce3d7ecab09ca84b6d0aaf69ffb3de44d7be6d
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-07-17 09:10:41 +0200

    passion simple gallery / responsive

--- js/passions.js
+++ js/passions.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import Gallery from "react-photo-gallery";
 import {photos} from "./photos/world";
 
-const Photo = () => <Gallery photos={photos} direction={"row"} />;
+const Photo = () => <Gallery photos={photos} direction={"column"} />;
 export default Photo;
--- js/photos/world.js
+++ js/photos/world.js
@@ -1,51 +1,69 @@
 export const photos = [
         {
             id: 1,
-            src: "../img/alpy.jpg",
+            src: "../img/irlandia.jpg",
             width: 4,
             height: 3
         },
         {
             id: 2,
-            src: "../img/irlandia.jpg",
-            width: 1,
-            height: 1
+            src: "../img/snieznik.jpg",
+            width: 4,
+            height: 3
         },
         {
             id: 3,
-            src: "../img/irlandia1.jpg",
-            width: 3,
-            height: 4
+            src: "../img/sopot.jpg",
+            width: 4,
+            height: 3
         },
         {
             id: 4,
-            src: "../img/snieznik.jpg",
-            width: 3,
-            height: 4
-        },
-        {
-            id: 5,
-            src: "../img/sopot.jpg",
-            width: 3,
-            height: 4
-        },
-        {
-            id: 6,
             src: "../img/wloch.jpg",
             width: 4,
             height: 3
         },
         {
-            id: 7,
+            id: 5,
             src: "../img/wroclaw.jpg",
-            width: 1,
-            height: 1
+            width: 3,
+            height: 4
+        },
+        {
+            id: 6,
+            src: "../img/alpy2.jpg",
+            width: 4,
+            height: 3
+        },
+        {
+            id: 7,
+            src: "../img/wroclaw1.jpg",
+            width: 3,
+            height: 4
         },
         {
             id: 8,
-            src: "../img/wroclaw1.jpg",
-            width: 1,
-            height: 1
+            src: "../img/bieszczady.jpg",
+            width: 4,
+            height: 3
+        },
+        {
+            id: 9,
+            src: "../img/irlandia2.jpg",
+            width: 3,
+            height: 4
+        },
+        {
+            id: 10,
+            src: "../img/alpy1.jpg",
+            width: 4,
+            height: 3
+        },
+        {
+            id: 11,
+            src: "../img/wilno.jpg",
+            width: 4,
+            height: 3
         }
     ];
 
