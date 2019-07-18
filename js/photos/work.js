6de3b3654521cb751c3434fa38dabe6b35350b91
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-07-18 12:48:56 +0200

    photos to portfolio

---
+++ js/photos/work.js
@@ -0,0 +1,152 @@
+export const works = [
+    {
+        id: 1,
+        src: "../img/wesela8.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 2,
+        src: "../img/wesela7.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 3,
+        src: "../img/wesela6.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 4,
+        src: "../img/wesela5.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 5,
+        src: "../img/wesela4.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 6,
+        src: "../img/wesela3.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 7,
+        src: "../img/wesela2.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 8,
+        src: "../img/wesela1.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 9,
+        src: "../img/wesela.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 10,
+        src: "../img/chrzest.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 11,
+        src: "../img/chrzest1.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 12,
+        src: "../img/chrzest2.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 13,
+        src: "../img/chrzest3.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 14,
+        src: "../img/rodzina.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 15,
+        src: "../img/rodzina1.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 16,
+        src: "../img/rodzina2.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 17,
+        src: "../img/rodzina3.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 18,
+        src: "../img/sluby.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 19,
+        src: "../img/sluby1.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 20,
+        src: "../img/sluby2.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 21,
+        src: "../img/sluby3.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 22,
+        src: "../img/sesje.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 23,
+        src: "../img/sesje1.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 24,
+        src: "../img/sesje2.jpg",
+        width: 3,
+        height: 4
+    },
+    {
+        id: 25,
+        src: "../img/sesje3.jpg",
+        width: 3,
+        height: 4
+    }
+    ];
